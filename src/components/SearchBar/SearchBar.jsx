6d46072c9018774512a0f7a31c29c43d62f48634
Sean May <s_may318@hotmail.com>
2021-11-11 14:33:16 -0500

    Changed the App to a class component no longer using react hooks, add a function to get a video from the search bar.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,28 +1,4 @@
 import React from "react";
-import useForm from "../UseForm/UseForm";
 
-const SearchBar = ({ onFormSubmit}) => {
-    const [term, setTerm] = useState("");
 
-    const onSubmit = (e) => {
-        e.preventDefault();
-        onFormSubmit(term);
-    };
 
-    return (
-        <div className="search-bar ui">
-            <form className="ui form" onSubmit={onSubmit}>
-                <div className="field">
-                    <label>Video Search</label>
-                    <input
-                        type="text"
-                        value={term}
-                        onChange={(e) => setTerm(e.target.value)}
-                    />
-                </div>
-            </form>
-        </div>
-    );
-};
-
-export default SearchBar;
