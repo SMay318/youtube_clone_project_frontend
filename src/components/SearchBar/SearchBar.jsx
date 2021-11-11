2a4fa0ccd3e2f547b59057a674104ccfe2d79af0
Sean May <s_may318@hotmail.com>
2021-11-11 11:51:00 -0500

    added a useForm component to be used on all forms that will be made for future use in this project.

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
+import useForm from "../UseForm/UseForm";
 
 const SearchBar = ({ onFormSubmit}) => {
     const [term, setTerm] = useState("");
