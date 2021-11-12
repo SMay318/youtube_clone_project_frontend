48cfc1c6f27120a706711ccffe93c52a7e9e305e
TylerSerrano98 <91335774+TylerSerrano98@users.noreply.github.com>
2021-11-12 15:50:51 -0600

    fixing merge conflict

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -31,21 +31,12 @@ class App extends Component {
         this.setState({
             videos: response.data.items,
             videoId: response.data.items[0].id.videoId
-<<<<<<< HEAD
-        })
-        this.getRelatedVideos(response.data.items[0].id.videoId)
+        },() => this.getRelatedVideos())
+        
     }
 
     getRelatedVideos = async (VideoId) => {
         let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedVideo=${VideoId}&part=snippet&type=video&key=${googleAPIKey}`)
-=======
-        },() => this.getRelatedVideos())
-        // this.getRelatedVideos(response.data.items[1].id.videoId)
-    }
-
-    getRelatedVideos = async () => {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?relatedToVideoId=${this.state.videoId}&part=snippet&type=video&key=${googleAPIKey}`)
->>>>>>> 88ccc4b0236877183898ace4f3f2590773c500b0
         console.log(response.data)
         this.setState({
             relatedVideos: response.data.items
