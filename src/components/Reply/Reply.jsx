c3debec38aaa9f12acfdcc1b2da4ebed803e0313
Sean May <s_may318@hotmail.com>
2021-11-15 17:35:53 -0500

    added view reply and add reply to the comment table to be used, still not functioning correctly.

--- src/components/Reply/Reply.jsx
+++ src/components/Reply/Reply.jsx
@@ -4,54 +4,34 @@ class Reply extends Component{
     constructor(props){
         super(props)
         this.state = {
-<<<<<<< HEAD
 
-            text:"",
-            video_id: props.video_id,
-=======
            
            reply: '',
            comment_id: this.props.comment_pk
->>>>>>> 6a14e289f567f32b1eae23164fbe2d76a5f96c7b
 
         }
     }
-
-    componentDidUpdate = (prevState) => {
-        if(
-            this.state.video_id != this.props.video_id
-        ){
-            this.setState({
-                videp_id: this.props.video_id
-            })
-        }
-    }
-
     handleChange = (event) => {
         console.log('hello')
         this.setState({
             [event.target.name]: event.target.value
         })
     }
-
     handleSubmit = async (event) => {
         event.preventDefault();
-        this.props.newReply(this.state)
+        this.props.createReply(this.state)
     }
-
     render() {
         return(
             <div>
-                <h4>Reply</h4>
-                <form onSubmit= {this.handleSubmit} method= 'post'>
+                <form onSubmit= {this.handleSubmit}>
                     <div>
-                        <input text= "text" name= "text" onChange={this.handleChange} value= {this.state.text} />
-                        <button type="submit" className="searchButton">Reply Here</button>
+                        <input text= "text" name= "reply" onChange={this.handleChange} />
+                        <button type="submit" className="searchButton">Add Reply</button>
                     </div>
                 </form>
             </div>
         )
     }
 }
-
 export default Reply;
